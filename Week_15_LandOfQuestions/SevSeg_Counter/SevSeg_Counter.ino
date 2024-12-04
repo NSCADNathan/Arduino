d3cda1c54daa1b5d50453e7aaa01be9e2e7d1b52
NSCADNathan <64323790+NSCADNathan@users.noreply.github.com>
2024-12-03 20:49:26 -0400

    updates

--- Week_15_LandOfQuestions/SevSeg_Counter/SevSeg_Counter.ino
+++ Week_15_LandOfQuestions/SevSeg_Counter/SevSeg_Counter.ino
@@ -35,7 +35,7 @@ void setup() {
   byte digitPins[] = {2};
   byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
   bool resistorsOnSegments = false; // 'false' means resistors are on digit pins
-  byte hardwareConfig = COMMON_CATHODE; // See README.md for options
+  byte hardwareConfig = COMMON_CATHODE; // !!!! >>>> THIS IS CHANGED FROM DEFAULT <<<< !!!!!
   bool updateWithDelays = false; // Default 'false' is Recommended
   bool leadingZeros = false; // Use 'true' if you'd like to keep the leading zeros
   bool disableDecPoint = false; // Use 'true' if your decimal point doesn't exist or isn't connected
