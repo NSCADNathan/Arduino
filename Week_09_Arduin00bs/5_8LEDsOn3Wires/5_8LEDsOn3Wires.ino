ce54c3eeaff68000fe3eef22147e0026c55bb686
NSCADNathan <64323790+NSCADNathan@users.noreply.github.com>
2025-10-27 16:24:13 -0300

    Fix include syntax and comment formatting

--- Week_09_Arduin00bs/5_8LEDsOn3Wires/5_8LEDsOn3Wires.ino
+++ Week_09_Arduin00bs/5_8LEDsOn3Wires/5_8LEDsOn3Wires.ino
@@ -5,11 +5,11 @@
   Released into the public domain.
 
 */
-#include ShiftRegister74HC595.h
+#include <ShiftRegister74HC595.h>
 
 // create a global shift register object
 // parameters number of shift registers (data pin, clock pin, latch pin)
-ShiftRegister74HC5951 sr(2, 3, 8);  custom pins
+ShiftRegister74HC5951 sr(2, 3, 8);  // custom pins
 
 void setup() {
 }
